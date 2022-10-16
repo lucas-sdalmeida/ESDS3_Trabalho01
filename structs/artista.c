9f06f30583e4f79fae8a4f5c70d0ef6e7e00b9b6
Lucas Silva de Almeida <lucas.lucas.sa@hotmail.com>
2022-10-16 15:47:58 -0300

    Implementacao_Artista
    Implementação das funções de artista.

---
+++ structs/artista.c
@@ -0,0 +1,39 @@
+#include "artista.h"
+
+Artista *novo_artista(char *nome, char *genero) {
+    if (!nome || strlen(nome) == 0)
+        return NULL;
+    if (!genero || strlen(genero) == 0)
+        return NULL;
+    
+    static int gerador_id = 0;
+    Artista *artista_novo = (Artista*)malloc(sizeof(Artista));
+
+    if (!artista_novo)
+        return NULL;
+
+    artista_novo->id = ++gerador_id;
+    strcpy(artista_novo->nome, nome);
+    strcpy(artista_novo->genero, genero);
+
+    return artista_novo;
+}
+
+int compara_artista(Artista *artista1, Artista *artista2) {
+    if (!artista1)
+        return -2;
+    if (!artista2)
+        return -3;
+    if (artista1->id > artista2->id)
+        return 1;
+    if (artista1->id < artista2->id)
+        return -1;
+    return 0;
+}
+
+Artista *apagar_artista(Artista *artista) {
+    if (!artista)
+        return -1;
+    free(artista);
+    return artista;
+}
--- structs/artista.h
+++ structs/artista.h
@@ -1,6 +1,9 @@
 #ifndef _ARTISTA_H
 #define _ARTISTA_H
 
+#include <stdlib.h>
+#include <string.h>
+
 #define TAMANHO_NOME_ARTISA 200
 #define TAMANHO_GENERO_ARTISTA 200
 
@@ -10,7 +13,7 @@ typedef struct artista {
     char genero[TAMANHO_GENERO_ARTISTA]
 } Artista;
 
-Artista *novo_artista(char *nome, char *titulo);
+Artista *novo_artista(char *nome, char *genero);
 
 int compara_artista(Artista *artista1, Artista *artista2);
 
